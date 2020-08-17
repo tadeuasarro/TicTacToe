f5c933a7da877b0a0f71bae4ab287f4a2cf4f3a7
tadeuasarro <tadeutas@gmail.com>
2020-08-17 19:06:02 -0300

    Start work on the round method

--- bin/main.rb
+++ bin/main.rb
@@ -12,11 +12,22 @@ class Game
   end
 
   def round
-
+      @board.display_board
+      @turn += 1
+      print "#{@player1.name}: " if @turn.odd?
+      print "#{@player2.name}: " if @turn.even?
+      puts "Which position to take?"
+      input = gets.chomp
+      if @board.include?(input.to_i)
+        #put some victory check method here
+      else
+        @turn -= 1
+        puts "Invalid play! Try again!"
+      end
   end
 
   def start_game
-    while turn < 9
+    while @turn < 9
       round
     end
   end
@@ -44,12 +55,10 @@ end
 new_board = Board[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 puts "First player? "
-first_player = Player.new(gets.chomp, 'X')
+first_player = Player.new(gets.chomp.capitalize, 'X')
 puts "Second player? "
-second_player = Player.new(gets.chomp, 'O')
+second_player = Player.new(gets.chomp.capitalize, 'O')
 
 new_game = Game.new(first_player, second_player, new_board)
 
-puts new_game.player1.name
-puts new_game.player2.name
-puts new_game.turn
+new_game.start_game
