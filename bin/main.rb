9fd512e802349cbc8c4e38a35ee45534b63c73b4
mateomh <mmojicah.84@gmail.com>
2020-08-17 09:48:49 -0500

    Add start of the game interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
 p 'Hello World From Brazil and Colombia'
+puts '****************************************************************'
+puts '*                    TIC TAC TOE THE GAME                      *'
+puts '****************************************************************'
+print "\n\n"
+puts ' Welcome to the TIC TAC TOE game. This is a two player game so'
+puts ' make sure you have a friend to play with.'
+puts ' The rules are simple: The first player to get 3 tokens in line'
+puts ' wins. Bring your A game'
+print "\n\n"
+
+loop do
+  print 'Do you want to start the game?[y/n]: '
+  init = gets.chomp
+  if init == 'n'
+    puts 'Thank you for playing'
+    return
+  end
+  break unless init != 'y' && init != 'n'
+end
