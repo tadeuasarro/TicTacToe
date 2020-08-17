abcfd8b371af9f3a9d734f19bf0e45b691ce95af
tadeuasarro <tadeutas@gmail.com>
2020-08-17 12:40:53 -0300

    Start add OOP to game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,45 @@
 #!/usr/bin/env ruby
+
+class Game
+  attr_accessor :turn, :board
+
+  def initialize
+    self.turn  = 0
+    self.board = Array(1..9)
+  end
+
+  def track_turns
+    self.turn.zero? ? self.turn=1 : self.turn=0
+  end
+
+  def check_victory_conditions
+  end
+
+  def check_position_availability
+  end
+
+end
+
+class Player < Game
+  attr_accessor :name, :token
+
+  def to_play
+  end
+
+end
+
+class Display < Game
+  def board_display
+  end
+
+  def message_display
+  end
+
+  def positive_negative_question
+  end
+
+end
+
 p 'Hello World From Brazil and Colombia'
 puts '****************************************************************'
 puts '*                    TIC TAC TOE THE GAME                      *'
@@ -21,16 +62,18 @@ loop do
 end
 
 puts "\n\n Lets get to know our participants!!!!!!\n\n"
-player1 = ''
-player2 = ''
+player1 = Player.new
+player2 = Player.new
 init=''
 loop do
   print 'Enter the name of player #1: '
-  player1 = gets.chomp
+  player1.name = gets.chomp
+  player1.token = 'X'
   print 'Enter the name of player #2: '
-  player2 = gets.chomp
-  puts "\n Welcome #{player1} your token will be X\n"
-  puts "\n Welcome #{player2} your token will be O\n\n"
+  player2.name = gets.chomp
+  player2.token = 'O'
+  puts "\n Welcome #{player1.name} your token will be X\n"
+  puts "\n Welcome #{player2.name} your token will be O\n\n"
   loop do
     print 'Is this information OK?[y/n]: '
     init = gets.chomp
@@ -48,7 +91,7 @@ puts "\n\n Lets begin our TIC TAC TOE GAME!!!!!!\n\n"
 board = Array(1..9)
 end_game = false
 loop do
-  print "\n\nIt's #{players[turn]} turn, THINK YOUR MOVE!!!!!!!\n"
+  print "\n\nIt's #{players[turn].name} turn, THINK YOUR MOVE!!!!!!!\n"
   print "\n\t---------------\n\t"
   board.each_with_index do |item, i|
     print "| #{item} |"
@@ -56,7 +99,7 @@ loop do
   end
   print "\n"
 
-  print " #{player1} Enter the position which you want to place your #{tokens[turn]}: "
+  print " #{players[turn].name} Enter the position which you want to place your #{tokens[turn]}: "
   move = gets.chomp.to_i
   next if move.zero? || move > 9 || move < 1
 
