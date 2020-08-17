cc99ae99a2fa7f63c106c02e4590a5ae1b80fc8b
mateomh <mmojicah.84@gmail.com>
2020-08-17 09:53:02 -0500

    Add player information interface

--- bin/main.rb
+++ bin/main.rb
@@ -19,3 +19,22 @@ loop do
   end
   break unless init != 'y' && init != 'n'
 end
+
+puts "\n\n Lets get to know our participants!!!!!!\n\n"
+player1 = ''
+player2 = ''
+init=''
+loop do
+  print 'Enter the name of player #1: '
+  player1 = gets.chomp
+  print 'Enter the name of player #2: '
+  player2 = gets.chomp
+  puts "\n Welcome #{player1} your token will be X\n"
+  puts "\n Welcome #{player2} your token will be O\n\n"
+  loop do
+    print 'Is this information OK?[y/n]: '
+    init = gets.chomp
+    break if init=='y' || init=='n'
+  end
+  break unless init != 'y'
+end
