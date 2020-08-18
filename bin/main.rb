b7bdd7bf2e71163d7ce35a30da2b990f3e8d421e
tadeuasarro <tadeutas@gmail.com>
2020-08-18 12:47:38 -0300

    First linters run fixing

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
+# rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength
 
 class Game
   attr_accessor :turn, :board
   attr_reader :player1, :player2
 
-  def initialize (player1 = Player, player2 = Player, board = Board)
+  def initialize(player1 = Player, player2 = Player, board = Board)
     @player1 = player1
     @player2 = player2
     @board = board
@@ -12,22 +13,22 @@ class Game
   end
 
   def round
-      @board.display_board
-      @turn += 1
-      win_pos = check_win_move(@player1.token) if @turn.odd?
-      win_pos = check_win_move(@player2.token) if @turn.even?
-      puts "YOU CAN WIN THIS!!!!!" if win_pos
-      print "#{@player1.name} (#{@player1.token}): " if @turn.odd?
-      print "#{@player2.name} (#{@player2.token}): " if @turn.even?
-      puts "Which position to take?"
-      input = gets.chomp.to_i
-      if @board.include?(input)
-        @board[input - 1] = "#{@player1.token}" if @turn.odd?
-        @board[input - 1] = "#{@player2.token}" if @turn.even?
-      else
-        @turn -= 1
-        puts "Invalid play! Try again!"
-      end
+    @board.display_board
+    @turn += 1
+    win_pos = check_win_move(@player1.token) if @turn.odd?
+    win_pos = check_win_move(@player2.token) if @turn.even?
+    puts 'YOU CAN WIN THIS!!!!!' if win_pos
+    print "#{@player1.name} (#{@player1.token}): " if @turn.odd?
+    print "#{@player2.name} (#{@player2.token}): " if @turn.even?
+    puts 'Which position to take?'
+    input = gets.chomp.to_i
+    if @board.include?(input)
+      @board[input - 1] = @player1.token if @turn.odd?
+      @board[input - 1] = @player2.token if @turn.even?
+    else
+      @turn -= 1
+      puts 'Invalid play! Try again!'
+    end
   end
 
   def start_game
@@ -37,7 +38,7 @@ class Game
       win = check_victory(@player2.token) if @turn.even?
       finish_game if win
     end
-    puts "NOBODY WON IT IS A DRAW"
+    puts 'NOBODY WON IT IS A DRAW'
   end
 
   def finish_game
@@ -48,14 +49,13 @@ class Game
   end
 
   def check_victory(token)
-    counters = [0,0,0,0] # [cols,rows,diag1,diag2]
+    counters = [0, 0, 0, 0] # [cols,rows,diag1,diag2]
     (0..2).each do |i|
-      counters[0]=[board[(i)],board[(i)+3],board[(i)+6]].count(token) # Checks the positions for the columns
-      counters[1]=[board[i*3],board[(i*3)+1],board[(i*3)+2]].count(token)  # Checks the positions for the rows
-      counters[2] += 1 if board[i*4] == token # Counts if a position in the main diagonal
-      counters[3] += 1 if board[(i*2)+2] == token # Counts if a position in the oposite diagonal
-      return true if counters.include? (3)
-
+      counters[0]=[board[(i)],board[(i) + 3],board[(i) + 6]].count(token) # Checks the positions for the columns
+      counters[1]=[board[i*3],board[(i * 3) + 1],board[(i * 3) + 2]].count(token)  # Checks the positions for the rows
+      counters[2] += 1 if board[i * 4] == token # Counts if a position in the main diagonal
+      counters[3] += 1 if board[(i * 2) + 2] == token # Counts if a position in the oposite diagonal
+      return true if counters.include? 3
     end
     false
   end
@@ -69,23 +69,21 @@ class Game
       row = [board[i * 3], board[(i * 3) + 1], board[(i * 3) + 2]] # Gets the whole row
       diag1.push(board[i * 4]) # Starts constructing the main diagonal
       diag2.push(board[(i * 2) + 2]) # Starts constructing the oposite diagonal
-      return true if col.count(token) == 2 && col.any?(Numeric) # If it has a number in it and two tokens is a winning move
+      return true if col.count(token) == 2 && col.any?(Numeric)
 
-      return true if row.count(token) == 2 && row.any?(Numeric) # If it has a number in it and two tokens is a winning move
-
+      return true if row.count(token) == 2 && row.any?(Numeric)
     end
-    return true if diag1.count(token) == 2 && diag1.any?(Numeric) # Checks the main diagonal for the winning move
+    return true if diag1.count(token) == 2 && diag1.any?(Numeric)
 
-    return true if diag2.count(token) == 2 && diag2.any?(Numeric) # Checks the oposite diagonal for the winning move
+    return true if diag2.count(token) == 2 && diag2.any?(Numeric)
 
     false
   end
-
 end
 
 class Player
-  attr_reader :name,:token
-  def initialize(name,token)
+  attr_reader :name, :token
+  def initialize(name, token)
     @name = name
     @token = token
   end
@@ -102,11 +100,13 @@ class Board < Array
   end
 end
 
+# rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength
+
 new_board = Board[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-puts "First player? "
+puts 'First player? '
 first_player = Player.new(gets.chomp.capitalize, 'X')
-puts "Second player? "
+puts 'Second player? '
 second_player = Player.new(gets.chomp.capitalize, 'O')
 
 new_game = Game.new(first_player, second_player, new_board)
