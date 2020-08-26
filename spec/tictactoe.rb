ab016b3234cf2c3209a9cf0f2b15cd4df1fb1822
tadeuasarro <tadeutas@gmail.com>
2020-08-26 18:26:59 -0300

    Add testcases for the board class

--- spec/tictactoe.rb
+++ spec/tictactoe.rb
@@ -2,3 +2,23 @@
 require './lib/board.rb'
 require './lib/player.rb'
 require './lib/game.rb'
+
+# BOARD CLASS
+describe Board do
+  describe '#take_position' do
+    it "Sets the first player's token (X) to the selected cell" do
+      board = Board.new
+      for i in (1..9) do
+        expect(board.take_position(i, 'X')).to eql('X')
+      end
+    end
+
+    it "Sets the second player's token (0) to the selected cell" do
+      board = Board.new
+      for i in (1..9) do
+        expect(board.take_position(i, '0')).to eql('0')
+      end
+    end
+
+  end
+end
